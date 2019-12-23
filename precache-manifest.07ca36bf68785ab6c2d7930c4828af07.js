2449b093f265c4f6653e173ea1bad19710fb85f8
bishtamit <bishtamit54@gmail.com>
2019-12-23 23:58:56 +0530

    Updates

---
+++ precache-manifest.07ca36bf68785ab6c2d7930c4828af07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b25487cf34fd104aab984b8ffb20c98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24f9ce5e536a9d2bc13f",
+    "url": "/static/css/main.4eb2d127.chunk.css"
+  },
+  {
+    "revision": "90f7cc4625644cda6d25",
+    "url": "/static/js/2.43da15e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.43da15e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24f9ce5e536a9d2bc13f",
+    "url": "/static/js/main.2ada657c.chunk.js"
+  },
+  {
+    "revision": "4b8f5b8baffab9d00115",
+    "url": "/static/js/runtime-main.81eab104.js"
+  }
+]);
